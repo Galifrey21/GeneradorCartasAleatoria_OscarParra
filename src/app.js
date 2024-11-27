3295bd144bb92bf4119cbe2581737216a680cf13
Oscar Parra <oscarparrapalomino@gmail.com>
2024-11-27 20:31:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let carta = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  function numeroAleatorio(arr) {
+    const aletoria = Math.floor(Math.random() * arr.length);
+    return aletoria;
+  }
+
+  let cartaRandom = carta[numeroAleatorio(carta)];
+  let paloRandom = palos[numeroAleatorio(palos)];
+  let classes = document.getElementsByClassName("suits");
+
+  for (let i = 0; i < classes.length; i++) {
+    classes[i].innerHTML = paloRandom;
+    if (paloRandom === "♦" || paloRandom === "♥") {
+      classes[i].classList.add("text-danger");
+    }
+  }
+
+  console.log(cartaRandom);
+  document.getElementById("cards").innerHTML = cartaRandom;
 };
